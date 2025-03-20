8f9a972865bf4af7b32915e97b98585af6cc209f
ehachmann <ehachmann@hotmail.com>
2025-03-20 20:11:10 -0300

    Versão 1: Entrada com captura do valor digitado

--- JogoDaAdvinhacao.Consoleapp/Program.cs
+++ JogoDaAdvinhacao.Consoleapp/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace JogoDaAdvinhacao.Consoleapp
+﻿namespace JogoDaAdivinhacao.Consoleapp
 {
     internal class Program
     {
+
+        //Versão 1: Estrutura básica e entrada do usuário
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("        Jogo da Adivinhação        ");
+            Console.WriteLine("-----------------------------------");
+
+            // Lógica do Jogo
+
+            Console.Write("Digite um número para chutar: ");
+            int numerodigitado = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Você digitou o número:  {numerodigitado}");
+            Console.ReadLine();
+
         }
     }
 }
